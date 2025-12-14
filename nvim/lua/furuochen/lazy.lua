5af73fd88b20f3bd2b122d10a085db7fc14fd5cf
Forsteri123 <furuochen.mail@gmail.com>
2025-12-14 17:29:48 +0800

    feat(nvim): better tree

--- nvim/lua/furuochen/lazy.lua
+++ nvim/lua/furuochen/lazy.lua
@@ -11,7 +11,8 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup({ { import = "furuochen.plugins" }, {import = "furuochen.plugins.lsp"} }, {
+require("lazy").setup(
+{ { import = "furuochen.plugins" }, { import = "furuochen.plugins.lsp" }, { import = "furuochen.plugins.snacks" } }, {
   change_detection = {
     notify = false,
   },
--- nvim/lua/furuochen/plugins/nvim-tree.lua
+++ nvim/lua/furuochen/plugins/nvim-tree.lua
@@ -28,10 +28,18 @@ return {
           enable = true,
         },
         icons = {
+          git_placement = "after",
+          show = {
+            folder_arrow = false
+          },
           glyphs = {
-            folder = {
-              arrow_closed = "", -- arrow when folder is closed
-              arrow_open = "", -- arrow when folder is open
+            git = {
+              staged = "w",
+              unstaged = "w",
+              deleted = "-",
+              untracked = "",
+              renamed = "",
+              ignored = "i"
             },
           },
         },
@@ -90,11 +98,12 @@ return {
     })
 
     -- set keymaps
-    local keymap = vim.keymap -- for conciseness
+    local keymap = vim
+        .keymap                                                                                                         -- for conciseness
 
-    keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" }) -- toggle file explorer
+    keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" })                         -- toggle file explorer
     keymap.set("n", "<leader>ef", "<cmd>NvimTreeFindFileToggle<CR>", { desc = "Toggle file explorer on current file" }) -- toggle file explorer on current file
-    keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file explorer" }) -- collapse file explorer
-    keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" }) -- refresh file explorer
+    keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file explorer" })                     -- collapse file explorer
+    keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" })                       -- refresh file explorer
   end
 }
